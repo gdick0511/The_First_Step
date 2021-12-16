5031aedaf0cf4ff5b7701b93973af98f69d55d65
garrettdick <garrettd0511@gmail.com>
2021-12-15 20:46:26 -0600

    edited the User model associations => class_name: 'Comment'

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2021_12_15_194505) do
   create_table "users", force: :cascade do |t|
     t.string "email"
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,45 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+User.destroy_all
+Post.destroy_all
+Comment.destroy_all
 
+puts "Seeding your User data now! 🌱"
 
-rails g resource User email username password 
+2.times do 
+User.create(
+    email: Faker::Internet.free_email,
+    username: Faker::FunnyName.two_word_name,
+    password_digest: Faker::Internet.password
+)
+end
 
-rails g resource Post subject body user:belongs_to
+puts "Seeding your Post data now! 📜"
 
-rails g resource Comment body user:belongs_to commentable:references{polymorphic}
+10.times do
+Post.create(
+    subject: Faker::Book.title,
+    body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    user_id: User.all.sample.id
+)
+end
+
+puts "Seeding your Comment data now! 📝"
+
+10.times do 
+Comment.create(
+    body: "Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum",
+    user_id: User.all.sample.id,
+    commentable_id: Post.all.sample.id,
+    commentable_type: "Post"
+)
+end
+
+puts "Done seeding 🎊"
+
+# rails g resource User email username password_digest 
+
+# rails g resource Post subject body user:belongs_to
+
+# rails g resource Comment body user:belongs_to commentable:references{polymorphic}
